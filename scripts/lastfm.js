b1356398b09c92e58547fcc36c3364d026cf8cf6
Rishi <riship4611@gmail.com>
2024-02-02 19:21:06 +0530

    added fm

--- scripts/lastfm.js
+++ scripts/lastfm.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const username = "riishiiiii"; // Replace with your Last.fm username
   const tracksContainer = document.getElementById("tracks-container");
 
-  fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json&limit=7`)
+  fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json&limit=8`)
     .then(response => response.json())
     .then(data => {
       data.recenttracks.track.slice(0, 10).forEach(track => {
@@ -15,15 +15,22 @@ document.addEventListener("DOMContentLoaded", function () {
         trackCard.classList.add("col");
 
         trackCard.innerHTML = `
-        <div class="card">
-          <img src="${imageUrl}" class="card-img-top" alt="${trackName} - ${artist}">
-          <div class="card-body" style="max-height: 100px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
-            <h5 class="card-title">${trackName}</h5>
-            <p class="card-text">${artist}</p>
+        <div class="card track-card">
+          <img class="card-img-top" src="${imageUrl}" alt="${trackName} - ${artist}">
+          <div class="card-body">
+            <h5 class="fs-6 card-title">${limitCharacters(trackName, 10)}</h5>
+            <p class="fs-6 card-text">${limitCharacters(artist, 10)}</p>
           </div>
         </div>
       `;
-
+      
+      // Function to limit characters and append "..."
+      function limitCharacters(input, maxLength) {
+        if (input.length > maxLength) {
+          return input.substring(0, maxLength) + "...";
+        }
+        return input;
+      }
         tracksContainer.appendChild(trackCard);
       });
     })
